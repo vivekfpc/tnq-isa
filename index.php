a4ba385da904ef75f8a268c87a56c49570a22593
Vivek Sharma V <vivek@fingerprintscreative.com>
2021-09-06 18:25:29 +0530

    Task Begin

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<title>TNQ - ISA</title>
+  	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400&family=Source+Sans+Pro:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<div class="top-banner">
+		<div class="container">
+			<div class="row top-header">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+					<div class="">
+						<img src="images/logo.png" />
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+					<button>SUBMIT YOUR ENTRY</button>
+				</div>
+			</div>
+			<h1>The Inspiring Science Award 2022<br> is open for entries and will close<br> on the 15th of october 2021</h1>
+			<p>500 entries received</p>
+			<button>VIEW ELIGIBILITY CRITERIA</button>
+		</div>
+	</div>
+	<div class="container">
+		<div class="timer">
+			<h4>Time Left for Submission</h4>
+		</div>
+	</div>
+</body>
+</html>
