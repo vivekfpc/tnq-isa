1e042a51f5d8e98205eddef1475899a21b4b664d
Vivek Sharma V <vivek@fingerprintscreative.com>
2021-09-14 15:38:27 +0530

    final-changes

--- index.php
+++ index.php
@@ -1,306 +1,616 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
+
 	<meta charset="UTF-8">
+
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   	<title>TNQ - ISA</title>
+
   	<!-- Latest compiled and minified CSS -->
+
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 	<!-- Optional theme -->
+
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
 	<link rel="shortcut icon" href="images/fav-icon.png" type="image/x-icon">
+
     <link rel="icon" href="images/fav-icon.png" type="image/x-icon">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 	<link rel="preconnect" href="https://fonts.googleapis.com">
+
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
 	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400&family=Poppins:wght@300&family=Source+Sans+Pro:wght@300;400;600&display=swap" rel="stylesheet">
+
 	<link rel="stylesheet" href="style/style.css">
+
 	<link rel="stylesheet" href="style/responsive-style.css">
+
 </head>
+
 <body>
+
 	<div class="top-banner">
+
 		<div class="container">
+
 			<div class="row top-header">
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-animate">
+
 					<img src="images/logo.svg"  class="img-responsive"/>
+
 				</div>
+
 				<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs text-animate">
+
 					<a href="https://isauat.tnq.co.in/register.php"><button>SUBMIT YOUR ENTRY</button></a>
+
 				</div>
+
 				<div class="hidden-lg hidden-md hidden-sm navbar-fixed-bottom">
+
 					<a href="https://isauat.tnq.co.in/register.php"><button>SUBMIT YOUR ENTRY</button></a>
+
 				</div>
+
 			</div>
+
 			<h1 class="text-animate">The Inspiring Science Award 2022</h1>
-			<h5 class="text-animate">is open for entries and will close on the 15th of october 2021</h5>
+
+			<h5 class="text-animate">is open for entries and will close at midnight, on October 15th, 2021</h5>
+
 			<p class="text-animate">500 entries received</p>
+
 			<div class="text-animate">
+
 			<button id="button">VIEW ELIGIBILITY CRITERIA</button></div>
+
 		</div>
+
 	</div>
+
 	<hr>
+
 	<div class="container text-animate">
+
 		<div class="timer">
+
 			<h4 id="headline">Entries close in</h4>
+
   				<div id="countdown">
+
    					<ul>
+
       					<li><span id="days"></span>days</li>
+
       					<li><span id="hours"></span>Hours</li>
+
       					<li><span id="minutes"></span>Minutes</li>
+
       					<li><span id="seconds"></span>Seconds</li>
+
     				</ul>
+
   				</div>
+
   				<div id="content" class="emoji">
+
     				<span></span>
+
   				</div>
+
 		</div>
+
 	</div>
+
 	<div class="scroll-1">
+
 		<div class="container">
+
 			<div class="row">
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 side">
+
 					<img src="images/scroll-1.png" class="img-responsive text-animate" />
+
 				</div>
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 side text-animate">
+
 					<div class="right">
+
 						<div class="line text-animate">
+
 							<h2>The Inspiring Science Award<br class="hidden-xs"> is for the best published<br class="hidden-xs"> Life Sciences paper by a<br class="hidden-xs"> student from India.</h2>
+
 						</div>
+
 					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</div>
+
 	<div class="history">
+
 		<div class="container">
+
 			<hr class="text-animate">
+
 			<h4 class="text-animate">History & Purpose</h4>
+
 			<hr class="text-animate">
+
 		</div>
+
 	</div>
+
 	<hr>
+
 	<div class="container">
+
 		<div class="history-btm">
-			<h3 class="text-animate">The jury and the sponsors aim to recognise and reward quality science, inspire scholarship, and support researchers to pursue their passion in the Life Sciences by selecting the best published scientific paper in the Life Sciences from India each year.</h3>
-			<p class="text-animate">The Inspiring Science Award was announced in January 2016, at The TNQ Distinguished Lectures in the Life Sciences in Bengaluru.  At the launch of the award by Dr Emilie Marcus, ex CEO, Cell Press, and Editor-in-Chief said “Building on the successful tradition of the lectureship in inspiring students to pursue their passion with careers in scientific research, we are creating The Inspiring Science Award in association with the lectures. <br><br>
+
+			<h3 class="text-animate">The Inspiring Science Award aims to recognise and reward quality science, inspire scholarship, and support researchers to pursue their passion in the Life Sciences by selecting the best published paper in the Life Sciences by a student from India each year.</h3>
+
+			<p class="text-animate">The Award was announced in January 2016, at The TNQ Distinguished in the Life Sciences in Bengaluru. At the launch, Dr Emilie Marcus, ex-CEO and Editor-in-Chief, Cell Press, said, “Building on the successful tradition of the lectureship in inspiring students to pursue their passion with careers in scientific research, we are creating The Inspiring Science Award in association with the lectures.<br><br>
+
+
 
 			The idea for this award was brought forth to us by Professor Anuranjan Anand of JNCASR and we would like to express our thanks and appreciation to him for helping extend the impact of the lecture series in promoting young Indian scientists."
+
 			</p>
+
 			<div class="row">
+
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 history-mobile">
+
 					<h2 class="text-animate">5</h2>
-					<h6 class="text-animate">Winners of the<br class="hidden-xs"> Inspiring Science award</h6>
+
+					<h6 class="text-animate">Winners of the<br class="hidden-xs"> Inspiring Science Award</h6>
+
 				</div>
+
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 history-mobile">
+
 					<h2 class="text-animate">1000+</h2>
+
 					<h6 class="text-animate">Entries since 2017</h6>
+
 				</div>
+
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 history-mobile">
+
 					<h2 class="text-animate">300+</h2>
+
 					<h6 class="text-animate">Research institutions<br class="hidden-xs"> represented</h6>
+
 				</div>
+
 			</div>
+
 			<div class="text-animate">
+
 			<button id="button-1">VIEW ELIGIBILITY CRITERIA</button></div>
+
 		</div>
+
 	</div>
+
 	<div class="prize">
+
 		<div class="container">
+
 			<hr class="text-animate">
+
 			<h4 class="text-animate">Prize</h4>
+
 			<hr class="text-animate">
-			<p class="text-animate">Eight papers are shortlisted by the jury for the top pick. All finalists receive an Apple laptop and<br class="hidden-xs"> a citation. The winner receives the ISA medal, a citation, and a travel fellowship to a<br class="hidden-xs"> conference of their choosing anywhere in the world. During the pandemic, and while travel<br class="hidden-xs"> is restricted, the winner receives a cash award of INR 2,00,000.</p>
+
+			<p class="text-animate">Eight papers are shortlisted by an eminent jury for the top pick. All finalists receive an Apple<br class="hidden-xs">laptop and a citation. The winner receives the ISA medal, a citation, and a travel fellowship to a<br class="hidden-xs">conference of their choosing anywhere in the world. During the pandemic, and while travel is<br class="hidden-xs"> restricted, the winner receives a cash award of INR 2,00,000 in lieu of the travel fellowship.</p>
+
 		</div>
+
 	</div>
+
 	<div class="eligibility" id="myDiv">
+
 		<div class="container">
+
 			<hr class="text-animate">
+
 			<h4 class="text-animate">Eligibility for 2022</h4>
+
 			<hr class="text-animate">
+
 			<div class="row list-mobile">
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
 					<ul>
+						<li class="text-animate">The Award is open to scientists who are registered for a PhD, all the way up to scientists in the 4th year of their post-doctorate degrees.</li>
+
 						<li class="text-animate">The research paper must have been published online for the first time between 1st October 2020 and 30th September 2021.</li>
+
 						<li class="text-animate">The paper should have already been accepted and published online in a journal either as an accepted manuscript publication or a corrected proof publication.</li>
+
 						<li class="text-animate">Only full length research papers are eligible to apply. Other article types such as Editorial, Review, etc. are not eligible.</li>
+
 						<li class="text-animate">The scientist must have been affiliated to a research institution or university based in India, where the research must have been conducted.</li>
+
+						
+
 					</ul>
+
 				</div>
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
 					<ul>
+		
 						<li class="text-animate">Papers with co-authors (Indian or international) are also eligible for consideration. However, the Award will be given only to the submitting author.</li>
-						<li class="text-animate">In cases where the paper has multiple authors, all other co-authors are required to submit a no-objection declaration to isa@tnq.co.in before the 20th of October 2020. If we do not hear from them by then, we will assume that they have no objection to your entry, and award, should your paper be chosen to win.</li>
+
+						<li class="text-animate">In cases where the paper has multiple authors, all other co-authors are required to submit a no-objection declaration to isa@tnq.co.in before the 20th of October 2021. If we do not hear from them by then, we will assume that they have no objection to your entry, and award, should your paper be chosen to win.</li>
+
 						<li class="text-animate">The paper must be uploaded as an attachment with your entry, supported by a proof of publication link. Entries as links to sites that are behind paywalls or firewalls are not acceptable.</li>
+
+						
 					</ul>
+
 				</div>
+
 			</div>
+
 			<a href="https://isauat.tnq.co.in/register.php">
+
 			<button class="text-animate">SUBMIT YOUR ENTRY</button></a>
+
 		</div>
+
 	</div>
+
 	<div class="winners">
+
 		<div class="container">
+
 			<hr class="text-animate">
+
 			<h4 class="text-animate">Past winners of the <br>Inspiring Science Award</h4>
+
 			<hr class="winner-hr text-animate">
+
 			<div class="row">
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2021.php"><img src="images/winner-1.png" class="img-responsive winner-1" /></a></div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2020.php"><img src="images/winner-2.png" class="img-responsive" /></a></div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2019.php"><img src="images/winner-3.png" class="img-responsive" /></a></div>
+
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2021.php" target="_blank"><img src="images/winner-1.png" class="img-responsive winner-1" /></a></div>
+
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2020.php" target="_blank"><img src="images/winner-2.png" class="img-responsive" /></a></div>
+
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2019.php" target="_blank"><img src="images/winner-3.png" class="img-responsive" /></a></div>
+
 			
+
 				<div class="col-lg-2 col-md-2 col-sm-2"></div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person-2 text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2018.php"><img src="images/winner-4.png" class="img-responsive" /></a></div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2017.php"><img src="images/winner-5.png" class="img-responsive" /></a></div>
+
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 winner-person-2 text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2018.php" target="_blank"><img src="images/winner-4.png" class="img-responsive" /></a></div>
+
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-animate"><a href="https://www.tnq.co.in/inspiring-science-award/2017.php" target="_blank"><img src="images/winner-5.png" class="img-responsive" /></a></div>
+
 				<div class="col-lg-2 col-md-2 col-sm-2"></div>
+
 			</div>
-			<h3 class="text-animate">"If you have questions, or are having trouble submitting your paper, please write to us: <a href="mailto:isa@tnq.co.in?subject=Enquiry">isa@tnq.co.in</a>"</h3>
+
+			<h3 class="text-animate">If you have questions, or are having trouble submitting your paper, please write to us: <a href="mailto:isa@tnq.co.in?subject=Enquiry">isa@tnq.co.in</a></h3>
+
 		</div>
+
 		<hr class="footer-line">
+
 		<div class="footer">
+
 			
+
 		<div class="container">
+
 			<div class="row">
-				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-8 left">
+
+				<div class="col-lg-1 col-md-1 col-sm-1 col-xs-2 left">
+
 					<ul>
-						<li><a href="https://www.tnq.co.in/" target="_blank"><img src="images/footer-logo.png" class="img-responsive text-animate-1" /><h5 class="text-animate-1">The Inspiring Science Award is <br class="hidden-xs">Sponsored by TNQ Technologies</h5></a></li>
+
+						<li><a href="https://www.tnq.co.in" target="_blank"><img src="images/footer-logo.png" class="text-animate-1" /></a></li>
+
 					</ul>
+
 				</div>
+				<div class="col-lg-5 col-md-5 col-sm-5 col-xs-6 left"><h5 class="text-animate-1">The Inspiring Science Award is <br class="hidden-xs">sponsored by TNQ Technologies</h5></div>
+
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-4 social-link">
+
 					<ul>
+
 						<li><a href="https://www.facebook.com/TheInspiringScienceAwards" target="_blank"><img src="images/fb.svg" class="img-responsive text-animate-1" /></a></li>
+
 						<li><a href="https://www.instagram.com/tnqdistinguishedlectures/" target="_blank"><img src="images/ln.svg" class="img-responsive text-animate-1" /></a></li>
+
 					</ul>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</div>
+
 	</div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+
+
+
 
 
 <script>
 
+
+
 ( function() { 
 
+
+
   // wait until all required libraries are available
+
   let chck_if_gsap_loaded = setInterval( function() {
+
     
+
     if( window.gsap && window.ScrollTrigger ) {
+
       
+
       // register scrolTrigger
+
       gsap.registerPlugin( ScrollTrigger );
 
+
+
       // ... do your thing
+
       my_stuff();
 
+
+
       // clear interval
+
       clearInterval( chck_if_gsap_loaded ); 
+
       
+
     }
+
     
+
   }, 500 );
+
   
+
   function my_stuff() {
+
 		
+
     
+
 	  
+
 	 
+
 	  ScrollTrigger.batch(".text-animate", {
+
         onEnter: batch => {
+
             gsap.from(batch, {
+
                 duration: 1,
+
                 y: 40,
+
                 scale: 1,
+
                 autoAlpha: 0,
+
                 ease: "Power2.easeOut",
+
                 stagger: 0.2,
+
                 visibility: "visible",
+
             })
+
         },
+
         toggleActions: "play none none none",
+
         once: true,
+
         start: 'top 80%',
+
         end: 'top 40%',
+
         markers: false,
 
 
+
+
+
     });
+
 	  ScrollTrigger.batch(".text-animate-1", {
+
         onEnter: batch => {
+
             gsap.from(batch, {
+
                 duration: 1,
+
                 y: 40,
+
                 scale: 1,
+
                 autoAlpha: 0,
+
                 ease: "Power2.easeOut",
+
                 stagger: 0.2,
+
                 visibility: "visible",
+
             })
+
         },
+
         toggleActions: "play none none none",
+
         once: true,
+
         start: 'top 100%',
+
         end: 'top 40%',
+
         markers: false,
 
 
+
+
+
     });
+
 	  
+
 	
+
 	  //End of My Stuff - Place code above here.
+
   }
+
   
+
 } )();
+
 </script>
 
 
+
+
+
 <script id="rendered-js" >
+
 (function () {
+
   const second = 1000,
+
   minute = second * 60,
+
   hour = minute * 60,
+
   day = hour * 24;
 
+
+
   let birthday = "Oct 15, 2021 23:59:59",
+
   countDown = new Date(birthday).getTime(),
+
   x = setInterval(function () {
 
+
+
     let now = new Date().getTime(),
+
     distance = countDown - now;
 
+
+
     document.getElementById("days").innerText = Math.floor(distance / day),
+
     document.getElementById("hours").innerText = Math.floor(distance % day / hour),
+
     document.getElementById("minutes").innerText = Math.floor(distance % hour / minute),
+
     document.getElementById("seconds").innerText = Math.floor(distance % minute / second);
 
+
+
     //do something later when date is reached
+
     if (distance < 0) {
+
       let headline = document.getElementById("headline"),
+
       countdown = document.getElementById("countdown"),
+
       content = document.getElementById("content");
 
+
+
       headline.innerText = "Entry has been closed";
+
       countdown.style.display = "none";
+
       content.style.display = "block";
 
+
+
       clearInterval(x);
+
     }
+
     //seconds
+
   }, 0);
+
 })();
+
 //# sourceURL=pen.js
+
     </script>
+
     <script>
+
     	$("#button, #button-1").click(function() {
+
     $('html, body').animate({
+
         scrollTop: $("#myDiv").offset().top
+
     }, 2000);
+
 });
+
     </script>
+
+
+
 
 
 </body>
+
 </html>
